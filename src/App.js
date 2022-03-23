388b036947b7e3c9e318f4255961895184e05009
kiki <kiki.zhang@dkdtrillion.com>
2022-03-23 15:24:35 +0800

    3-Header组件使用react-redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { Provider } from 'react-redux'
+import store from './store';
 import Header from "./common/header";
 
 function App() {
   return (
-    <div>
+    <Provider store={store}>
       <Header />
-    </div>
+    </Provider>
   );
 }
 
--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { CSSTransition } from 'react-transition-group'
 import {
   HeaderWrapper,
@@ -11,66 +12,65 @@ import {
   Button
 } from './style'
 
-class Header extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      focused: false 
-    }
+// 无状态组件 - 提高性能
+const Header = props => {
+  return (
+    <HeaderWrapper>
+      <Logo />
+      <Nav>
+        <NavItem className='left active'>首页</NavItem>
+        <NavItem className='left'>下载App</NavItem>
+        <NavItem className='right'>
+          <span className='iconfont'>&#xe636;</span>
+        </NavItem>
+        <NavItem className='right'>登陆</NavItem>
+        <SearchWrapper>
+          <CSSTransition
+            in={props.focused}
+            timeout={200}
+            classNames="slide"
+          >
+            <NavSearch
+              className={props.focused ? 'focused' : ''}
+              onFocus={props.handleInputFocus}
+              onBlur={props.handleInputBlur}
+            ></NavSearch>
+          </CSSTransition>
+          {/* <span className={[props.focused ? 'focused' : '', 'iconfont search'].join(' ')}>&#xe60c;</span> */}
+          <span className={`iconfont search ${props.focused ? 'focused' : ''}`}>&#xe60c;</span>
+        </SearchWrapper>
+      </Nav>
+      <Addition>
+        <Button className='reg'>注册</Button>
+        <Button className='writting'>
+          <span className='iconfont pencil'>&#xe6eb;</span>
+          写文章
+        </Button>
+      </Addition>
+    </HeaderWrapper>
+  )
+}
 
-    this.handleInputFocus = this.handleInputFocus.bind(this)
-    this.handleInputBlur = this.handleInputBlur.bind(this)
-  }
-
-  render() {
-    return (
-      <HeaderWrapper>
-        <Logo />
-        <Nav>
-          <NavItem className='left active'>首页</NavItem>
-          <NavItem className='left'>下载App</NavItem>
-          <NavItem className='right'>
-            <span className='iconfont'>&#xe636;</span>
-          </NavItem>
-          <NavItem className='right'>登陆</NavItem>
-          <SearchWrapper>
-            <CSSTransition
-              in={this.state.focused}
-              timeout={200}
-              classNames="slide"
-            >
-              <NavSearch
-                className={this.state.focused ? 'focused' : ''}
-                onFocus={this.handleInputFocus}
-                onBlur={this.handleInputBlur}
-              ></NavSearch>
-            </CSSTransition>
-            {/* <span className={[this.state.focused ? 'focused' : '', 'iconfont search'].join(' ')}>&#xe60c;</span> */}
-            <span className={`iconfont search ${this.state.focused ? 'focused' : ''}`}>&#xe60c;</span>
-          </SearchWrapper>
-        </Nav>
-        <Addition>
-          <Button className='reg'>注册</Button>
-          <Button className='writting'>
-            <span className='iconfont pencil'>&#xe6eb;</span>
-            写文章
-          </Button>
-        </Addition>
-      </HeaderWrapper>
-    )
-  }
-
-  handleInputFocus() {
-    this.setState({
-      focused: true
-    })
-  }
-
-  handleInputBlur() {
-    this.setState({
-      focused: false
-    })
+const mapStateToProps = (state) => {
+  return {
+    focused: state.focused
   }
 }
 
-export default Header
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleInputFocus() {
+      const action = {
+        type: 'search_focus'
+      }
+      dispatch(action)
+    },
+    handleInputBlur() {
+      const action = {
+        type: 'search_blur'
+      }
+      dispatch(action)
+    }
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
