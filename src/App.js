4c0d5bf96546ea4913b4c0eb3152c9e41f58bbd2
kiki <kiki.zhang@dkdtrillion.com>
2022-04-18 09:40:53 +0800

    0418-01-react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import { React, Component } from 'react'
 import { Provider } from 'react-redux'
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import store from './store';
 import Header from "./common/header";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>home</div>
+    )
+  }
+}
+class Detail extends Component {
+  render() {
+    return (
+      <div>detail</div>
+    )
+  }
+}
 
 function App() {
   return (
     <Provider store={store}>
       <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />}></Route>
+          <Route path="/detail" exact element={<Detail />}></Route>
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 }
